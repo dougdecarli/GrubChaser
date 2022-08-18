d51fb68d102dcacfd2ba46edc86624744ac38df6
Douglas de Carli <douglas.immig@cwi.com.br>
2022-08-18 16:21:39 -0300

    first commit

---
+++ GrubChaserUITests/GrubChaserUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GrubChaserUITestsLaunchTests.swift
+//  GrubChaserUITests
+//
+//  Created by Douglas Immig on 18/08/22.
+//
+
+import XCTest
+
+class GrubChaserUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
